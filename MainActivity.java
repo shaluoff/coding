eeeb8e5941a5958a551349ccedc940713f0ccdae
shaluoff <101888947+shaluoff@users.noreply.github.com>
2022-03-22 10:14:19 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.loginsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+ Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.buttonnav);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
